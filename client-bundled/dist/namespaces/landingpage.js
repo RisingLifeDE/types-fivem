3630fb5671fcc569c522b50213061652e20a81f0
paul <elrobtossohn@gmail.com>
2026-07-21 05:05:05 +0200

    chore: updated to latest native changes

---
+++ client-bundled/dist/namespaces/landingpage.js
@@ -0,0 +1,44 @@
+/**
+ * No comment provided
+ *
+ * Hash: 0x770CB76ED0895B5B | Since: unknown | API-Set: unknown
+ */
+export function getSelectedCharacterSlot() {
+    if (!IsGameEnhancedVersion())
+        console.error('This native is not supported on legacy (0x770CB76ED0895B5B)');
+    return;
+    return Citizen.invokeNative('0x770CB76ED0895B5B');
+}
+/**
+ * No comment provided
+ *
+ * Hash: 0xB37FC2FB7418367C | Since: unknown | API-Set: unknown
+ */
+export function isActive() {
+    if (!IsGameEnhancedVersion())
+        console.error('This native is not supported on legacy (0xB37FC2FB7418367C)');
+    return;
+    return Citizen.invokeNative('0xB37FC2FB7418367C');
+}
+/**
+ * No comment provided
+ *
+ * Hash: 0x237D6F6D474FC93C | Since: unknown | API-Set: unknown
+ */
+export function setShouldDismiss() {
+    if (!IsGameEnhancedVersion())
+        console.error('This native is not supported on legacy (0x237D6F6D474FC93C)');
+    return;
+    Citizen.invokeNative('0x237D6F6D474FC93C');
+}
+/**
+ * No comment provided
+ *
+ * Hash: 0xFD1B81BDD7FBCB41 | Since: unknown | API-Set: unknown
+ */
+export function setShouldLaunch(entrypointId) {
+    if (!IsGameEnhancedVersion())
+        console.error('This native is not supported on legacy (0xFD1B81BDD7FBCB41)');
+    return;
+    Citizen.invokeNative('0xFD1B81BDD7FBCB41', entrypointId);
+}
---
+++ natives/dist/namespaces/landingpage.d.ts
@@ -0,0 +1,24 @@
+/**
+ * No comment provided
+ *
+ * Hash: 0x770CB76ED0895B5B | Since: unknown
+ */
+export declare function getSelectedCharacterSlot(): number;
+/**
+ * No comment provided
+ *
+ * Hash: 0xB37FC2FB7418367C | Since: unknown
+ */
+export declare function isActive(): boolean;
+/**
+ * No comment provided
+ *
+ * Hash: 0x237D6F6D474FC93C | Since: unknown
+ */
+export declare function setShouldDismiss(): void;
+/**
+ * No comment provided
+ *
+ * Hash: 0xFD1B81BDD7FBCB41 | Since: unknown
+ */
+export declare function setShouldLaunch(entrypointId: number): void;
